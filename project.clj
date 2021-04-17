1de75da85b2a4f23a6f0219f190b0821d6ba6a3e
alexpetrov <alexpetrov.rb@gmail.com>
2021-04-17 23:00:40 +0300

    Json support

--- project.clj
+++ project.clj
@@ -4,13 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/java.jdbc "0.7.5"]]
-  :pedantic? :abort
+                 [org.clojure/java.jdbc "0.7.5"]
+                 [org.clojure/data.json "2.2.0"]
+                 [org.postgresql/postgresql "42.2.16"]]
+  ;;:pedantic? :abort
   :profiles {:dev {:source-paths ["dev"]
                    :repl-options {:init-ns user}
                    :dependencies [[org.clojure/tools.namespace "0.2.11"]
-                                  [org.clojure/test.check "0.9.0"]
-                                  [org.postgresql/postgresql "42.1.4"]]
+                                  [org.clojure/test.check "0.9.0"]]
                    :plugins [[com.jakemccrary/lein-test-refresh "0.18.1"]]}}
   :deploy-repositories [["releases" {:url "https://clojars.org/repo/",
                                      :creds :gpg}]])
